760f3f70e1d328f5d51c7f2c2550dbf963bd2cc1
igorsrajer123 <igorsrajer123@gmail.com>
2021-01-08 18:31:22 +0100

    Initial project upload

---
+++ pharmacySystem/src/main/java/com/example/pharmacySystem/PharmacySystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmacySystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacySystemApplication.class, args);
+	}
+
+}
